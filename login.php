a0e2584f540a9ba6377f902d8c2a5f75c7645716
Aymo <96393445+NotAymo@users.noreply.github.com>
2024-05-08 21:49:31 +0200

    DO PUSHUPS DAMMIT

---
+++ login.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In - Onyx</title>
+    <link rel="stylesheet" href="https://cloned.pizza/assets/css/onyx.css">
+</head>
+<body>
+    <h1>Log into your Onyx account</h1>
+    <p>Username</p>
+    <form action="login1.php" method="get">
+        <input type="text" id="usrnm">
+        <input type="password" id="pswrd">
+    </form>
+    <p>No account? Create one <a href="signup.php"><p>here</p></a></p>
+</body>
+</html>
